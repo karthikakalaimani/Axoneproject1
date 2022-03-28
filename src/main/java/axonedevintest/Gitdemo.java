98e875ed2cc43d2c76fa9988cd425367850f2472
Karthikakalaimani <karthikakalaimani10@gmail.com>
2022-03-28 18:12:50 +0100

    jIRA 1234--> Committing the project to GIT repository

---
+++ src/main/java/axonedevintest/Gitdemo.java
@@ -0,0 +1,7 @@
+package axonedevintest;
+
+public class Gitdemo {
+    public static void main(String[] args) {
+        System.out.println("Got is cool");
+    }
+}
---
+++ src/test/java/uk/axone/devintest/Gittest.java
@@ -0,0 +1,24 @@
+package uk.axone.devintest;
+
+import org.testng.annotations.Test;
+
+public class Gittest {
+    @Test
+    public void test1() {
+        System.out.println("Running Test 1");
+    }
+    @Test
+    public void test2(){
+        System.out.println("Running Test 2");
+    }
+    @Test
+    public void test3(){
+        System.out.println("Running Test 3");
+    }
+    @Test
+
+    public void test4(){
+        System.out.println("Running Test 4");
+    }
+
+}
